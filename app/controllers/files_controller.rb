d937593b01cda0a9f2d9502a33358572e66eec16
islam Gebriel <iGebriel@gmail.com>
2021-09-14 02:12:16 +0200

    adding controller and view files

---
+++ app/controllers/files_controller.rb
@@ -0,0 +1,30 @@
+class FilesController < ApplicationController
+  def new
+  end
+
+  def index
+    @pe_files = PortableExecutable.includes(:file_attachment).all.order('updated_at DESC')
+  end
+
+  def create
+    pe = PE.new(params[:pe_file].tempfile)
+    if pe.pe? 
+      uploaded = PortableExecutable.already_uploaded? pe.information
+      if uploaded
+        uploaded.recieved_again
+        flash[:notice] = 'file has been uploaded before'
+        return redirect_to action: :index
+      end
+      uploaded = PortableExecutable.create(pe.information)
+      uploaded.file.attach params[:pe_file]
+      flash[:success] = 'file has been uploaded successfully'
+      return redirect_to action: :index
+    end
+    flash[:error] = 'not a PE file'
+    return redirect_to action: :index
+  end
+
+  private
+
+
+end
